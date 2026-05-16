7494e1334cb526d5fd3d11004bbbeaa02bd8a058
Monish Shrestha <shrestha.sumit1371@gmail.com>
2026-05-17 01:01:48 +0545

    created footer and home page layout

--- Frontend/src/components/layout/Footer.tsx
+++ Frontend/src/components/layout/Footer.tsx
@@ -1,5 +1,89 @@
 export default function Footer() {
   return (
-    <div className="] w-full bg-[#EEEFFB] h-60 mt-30">Footer</div>
-  )
+    <footer className="w-full bg-[#eeeffb] pt-16 mt-100 ">
+      <div className="  container lg:px-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 ">
+
+        {/* LEFT */}
+        <div>
+          <h2 className="text-2xl font-bold mb-5">Hekto</h2>
+
+          {/* Email */}
+          <div className="flex mb-4">
+            <input
+              type="email"
+              placeholder="Enter Email Address"
+              className="px-2 py-2 w-full text-sm outline-none bg-[#FFFFFF]"
+            />
+            <button className="bg-pink-500 text-white px-6 font-lato text-[16px] whitespace-nowrap rounded-sm">
+              Sign Up
+            </button>
+          </div>
+
+          <p className="text-gray-500 text-sm mt-4">Contact Info</p>
+          <p className="text-gray-400 text-sm mt-2 leading-6">
+            Budanilkantha, Kathmandu 11, Nepal
+          </p>
+        </div>
+
+        {/* Categories */}
+        <div>
+          <h3 className="font-semibold mb-5">Catagories</h3>
+          <ul className="space-y-3 text-sm text-gray-400">
+            <li>Laptops & Computers</li>
+            <li>Cameras & Photography</li>
+            <li>Smart Phones & Tablets</li>
+            <li>Video Games & Consoles</li>
+            <li>Waterproof Headphones</li>
+          </ul>
+        </div>
+
+        {/* Customer Care */}
+        <div>
+          <h3 className="font-semibold mb-5">Customer Care</h3>
+          <ul className="space-y-3 text-sm text-gray-400">
+            <li>My Account</li>
+            <li>Discount</li>
+            <li>Returns</li>
+            <li>Orders History</li>
+            <li>Order Tracking</li>
+          </ul>
+        </div>
+
+        {/* Pages */}
+        <div>
+          <h3 className="font-semibold mb-5">Pages</h3>
+          <ul className="space-y-3 text-sm text-gray-400">
+            <li>Blog</li>
+            <li>Browse the Shop</li>
+            <li>Category</li>
+            <li>Pre-Built Pages</li>
+            <li>Visual Composer Elements</li>
+            <li>WooCommerce Pages</li>
+          </ul>
+        </div>
+      </div>
+
+      {/* Bottom bar */}
+      <div className="mt-12 border-t border-gray-200 py-4 track bg-[#E7E4F8]">
+        <div className="max-w-7xl mx-auto px-6 lg:px-12 flex flex-col md:flex-row justify-between items-center text-sm text-gray-400">
+
+          <p>©MonishShrestha - All Rights Reserved</p>
+
+          {/* Social */}
+          <div className="flex gap-3 mt-3 md:mt-0">
+            <div className="w-7 h-7 flex items-center justify-center rounded-full bg-[#3b5998] text-white text-xs">
+              F
+            </div>
+            <div className="w-7 h-7 flex items-center justify-center rounded-full bg-[#ab239b] text-white text-xs">
+              I
+            </div>
+            <div className="w-7 h-7 flex items-center justify-center rounded-full bg-[#000000] text-white text-xs">
+              X
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </footer>
+  );
 }
--- Frontend/src/pages/Home.tsx
+++ Frontend/src/pages/Home.tsx
@@ -1,7 +1,75 @@
-
+import Sofa from "../assets/home/Sofa.png";
+import Lamp from "../assets/home/Hanging lamp.png";
+import Discount from "../assets/home/Discount.png";
 
 export default function Home() {
   return (
-  <div>Home</div>
-  )
+    <section className="w-full bg-[#f2f0ff] overflow-hidden">
+      <div className="  container   min-h-187.5 lg:px-12 flex items-center ">
+
+        {/* LEFT */}
+        <div className="flex-1 relative max-w-275">
+
+          {/* Lamp */}
+          <img
+            src={Lamp}
+            alt="lamp"
+            className="hidden lg:block absolute -top-50 -left-32 w-65"
+          />
+
+          {/* Small text */}
+          <p className="text-pink-500 font-semibold text-sm mb-3 ml-20">
+            Best Furniture For Your Castle....
+          </p>
+
+          {/* Heading */}
+          <h1 className="text-[30px] md:text-[53px] leading-tight font-bold  text-black font-josefins ml-20 ">
+            New Furniture Collection Trends in 2020
+          </h1>
+
+          {/* Desc */}
+          <p className="text-[#8A8FB9]mt-5 max-w-md text-[15px] leading-7 ml-20">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Magna in est adipiscing in phasellus non in justo.
+          </p>
+
+          {/* Button */}
+          <button className="mt-7 bg-pink-500 hover:bg-pink-600 text-white px-7 py-3 rounded-md font-semibold shadow-md ml-20">
+            Shop Now
+          </button>
+
+          {/* Dot */}
+          <div className="w-3 h-3 bg-pink-500 rounded-full mt-16 ml-2"></div>
+        </div>
+
+        {/* RIGHT */}
+        <div className="flex-1 relative flex justify-center items-center">
+
+          {/* Circle */}
+          <div className="absolute w-137.5 h-137.5 bg-[#ECD2FA] rounded-full opacity-60"></div>
+
+          {/* Sofa */}
+          <img
+            src={Sofa}
+            alt="sofa"
+            className="relative z-10 w-120 "
+          />
+
+          {/* Discount */}
+          <img
+            src={Discount}
+            alt="discount"
+            className="absolute top-3 right-5 z-20"
+          />
+        </div>
+      </div>
+
+      {/* Bottom dots */}
+      <div className="flex justify-center gap-2 pb-6">
+        <div className="w-3 h-3 bg-pink-500 rounded-full"></div>
+        <div className="w-3 h-3 border border-pink-500 rounded-full"></div>
+        <div className="w-3 h-3 border border-pink-500 rounded-full"></div>
+      </div>
+    </section>
+  );
 }
