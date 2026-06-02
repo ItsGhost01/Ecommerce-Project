26aae745a4a1f686d69cc7269a982d7c895280cb
Monish Shrestha <shrestha.sumit1371@gmail.com>
2026-06-03 00:27:59 +0545

    fixed post logic for cart

--- Backend/src/controllers/cart.ts
+++ Backend/src/controllers/cart.ts
@@ -37,18 +37,33 @@ export const addToCart = async (req: Request, res: Response) => {
 
    
   try {
-    const { id, productId, quantity, userId } = req.body;
 
-    const cart = await Cart.create({
-      productId: req.body.productId,
-      userId: req.user?.id,
-      quantity: req.body.quantity,
-    });
+    let existingCartItem = await Cart.findOne({
+        where:{
+            userId: req.user?.id,
+            productId: req.body.productId
+        }
+    })
+    let data;
+    if(existingCartItem){
+       data = await existingCartItem.update({
+            quantity: req.body.quantity
+        })
+    } else {
+
+        const { id, productId, quantity, userId } = req.body;
+    
+        data = await Cart.create({
+          productId: req.body.productId,
+          userId: req.user?.id,
+          quantity: req.body.quantity,
+        });
+    }
 
     res.status(201).json({
       success: true,
       message: "Added to Cart successfully",
-      data: cart,
+      data: Cart,
     });
   } catch (error) {
     console.log(error);
